ff559ed29bea6a0a797ad7ba88dfdcc3fa73bf83
Neil Doherty <neildohertyjr@gmail.com>
2022-07-28 11:50:43 -0500

    Initial Commit

---
+++ Hacker News Reader/Hacker_News_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_ReaderApp.swift
+//  Hacker News Reader
+//
+//  Created by user220431 on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
